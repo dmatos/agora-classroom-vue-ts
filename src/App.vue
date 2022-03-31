ded5eb30c7ac1edcd487b4d0713996571fe24f10
dmatos <dmatos88@gmail.com>
2022-03-31 01:40:35 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import {AgoraEduSDK} from 'agora-classroom-sdk'
+
+// Configure courseware
+let resourceUuid = "xxxxx"
+let resourceName = "my ppt slide"
+let sceneInfos = []
+let sceneInfo = {
+    name: "1",
+    ppt: {
+        src: "pptx://....",
+        width: 480,
+        height: 360
+    }
+}
+sceneInfos.push(sceneInfo)
+
+let courseWareList = [{
+    resourceUuid,
+    resourceName,
+    size: 10000,
+    updateTime: new Date().getTime(),
+    ext: "pptx",
+    url:null,
+    scenes: sceneInfos,
+    taskUuid: "xxxx",
+    taskToken: "xxx",
+    taskProgress: NetlessTaskProgress
+}]
+
+// Launch a classroom
+AgoraEduSDK.launch(document.querySelector(`#${this.elem.id}`), {
+    rtmToken: "<your rtm token>",
+    userUuid: "test",
+    userName: "teacher",
+    roomUuid: "4321",
+    roleType: 1,
+    roomType: 4,
+    roomName: "demo-class",
+    pretest: false,
+    language: "en",
+    startTime: new Date().getTime(),
+    duration: 60 * 30,
+    courseWareList: [],
+    listener: (evt) => {
+        console.log("evt", evt)
+    }
+})
+
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
